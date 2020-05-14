29c7516e20d70aca06111006ee2b48f9edba3d88
Adam Korczynski <adam@adalogics.com>
2020-05-14 15:23:27 +0100

    Gofmt'd fuzzer

--- cryptoservice/fuzz/fuzz.go
+++ cryptoservice/fuzz/fuzz.go
@@ -1,17 +1,17 @@
 package fuzz
 
 import (
-        "github.com/theupdateframework/notary/trustmanager"
-        "github.com/theupdateframework/notary/cryptoservice"
-        "github.com/theupdateframework/notary/passphrase"
+	"github.com/theupdateframework/notary/cryptoservice"
+	"github.com/theupdateframework/notary/passphrase"
+	"github.com/theupdateframework/notary/trustmanager"
 )
 
 // Fuzz implements the fuzzer that targets GetPrivateKey
 func Fuzz(data []byte) int {
-        cryptos := cryptoservice.NewCryptoService(trustmanager.NewKeyMemoryStore(passphrase.ConstantRetriever("pass")))
-        _, _, err := cryptos.GetPrivateKey(string(data))
-        if err != nil {
-                return 0
-        }
-        return 1
+	cryptos := cryptoservice.NewCryptoService(trustmanager.NewKeyMemoryStore(passphrase.ConstantRetriever("pass")))
+	_, _, err := cryptos.GetPrivateKey(string(data))
+	if err != nil {
+		return 0
+	}
+	return 1
 }
